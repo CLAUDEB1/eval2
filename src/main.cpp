777192ef48c50ca77453a7aa2bd7ae901ada1e38
CLAUDEB1 <claudebenjamin255@gmail.com>
2023-12-15 12:54:58 +0000

    Evaluation n°2

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  // variable permettant de compter le nombre de places disponibles
+  int nbrePlaces = 10;
+  // variable permettant de récupérer le choix de l'utilisateur
+  int choix;
+  cout << "************ Evaluation 2 ************" << endl;
+  cout << "1 - Entrée dans le parking\n" << std::endl;
+  cout << "2 - Nombre de place disponible\n" << endl;
+  cout << "3 - Sortie du parking\n" << endl;
+  cout << "4 - Quitter\n" << endl;
+}
